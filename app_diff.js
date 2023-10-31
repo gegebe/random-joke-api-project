7992c099f29b17b9746814838596c7d98abd94a5
gegebe <ggirbes@gmail.com>
2023-10-31 09:21:25 +0100

    finished

--- app_diff.js
+++ app_diff.js
@@ -3,18 +3,12 @@ const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
 
-//Las peticiones GET tardan x ms, por tanto ponemos await
 async function getRandomJoke(){
-
-  //1. Obtener datos de la API
   const respuesta = await fetch("https://api.chucknorris.io/jokes/random");
-
-  // 2. Transformarlos en algo que pueda manipular con JS (objeto, array, array de objetos)
   let datos = await respuesta.json();
-
-  //3. Actualizar el DOM
-  jokeDIV.textContent = datos.value;
-  
+  return datos.value;
 }
 
-button.addEventListener("click", getRandomJoke);
+button.addEventListener("click", async function(){
+  jokeDIV.textContent = await getRandomJoke();
+});
